3443084181e6e2d503343e6ef3fe11603127b7f5
Justin Spiegel <justin@Justins-Air.home>
2018-05-18 03:27:15 -0400

    Initial commit

---
+++ lastfm_run.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+from lastfmwrapper import LastFmWrapper
+
+bot = commands.Bot(command_prefix='$')
+lastfm = LastFmWrapper()
+
+def read_username_file(username_dict):
+    reader = open("usernames.txt", 'r')
+    lines = reader.readlines()
+    for line in lines:
+        author = line.split(",")[0]
+        username = line.split(",")[1]
+        if lastfm.get_user(username) != None:
+            username_dict[author] = username
+    reader.close()
+
+def rewrite_username_file(username_dict):
+    writer = open("usernames.txt", 'w')
+    for author, username in username_dict.items():
+        writer.write(author + "," + username + "\n")
+    writer.close()
+    
+username_dict = {}
+read_username_file(username_dict)
+
+@bot.command(pass_context=True)
+@commands.cooldown(1, 300, commands.BucketType.user)
+async def fm(ctx):
+    author = str(ctx.message.author)
+    if author not in username_dict:
+        await bot.say("Set a username first using $fmset.")
+        return
+    else:
+        username = username_dict[author]
+    
+    now_playing = lastfm.get_now_playing(username)
+    if now_playing == None:
+        await bot.say(username + " has never played any songs.")
+        return
+
+    embed = discord.Embed(colour=0xFF0000, title=now_playing.title, description=now_playing.artist.name)
+    embed.set_author(name=username, icon_url=ctx.message.author.avatar_url, url="https://www.last.fm/user/" + username)
+    embed.set_thumbnail(url=now_playing.image)
+
+    await bot.say(embed=embed)
+
+@fm.error
+async def fm_error(error, ctx):
+    if isinstance(error, commands.CommandOnCooldown):
+        await bot.say("Wait {}m, {}s for the cooldown, you neanderthal.".format(int(error.retry_after / 60), int(error.retry_after) % 60))
+    else:
+        await bot.say("Unknown error occurred. You done fucked up big time.")
+
+@bot.command(pass_context=True)
+async def fmset(ctx, username):
+    author = str(ctx.message.author)
+    if lastfm.get_user(username) != None:
+        username_dict[author] = username
+        await bot.say("Username set.")
+        rewrite_username_file(username_dict)
+    else:
+        await bot.say("User not found.")
+
+bot.run('NDQ1ODQzODMwODYwOTM5MjY1.DdzE-g.kffUonxFS9M-0OMCUcwnAYErGYQ')
---
+++ lastfmwrapper.py
@@ -0,0 +1,67 @@
+import time
+import os
+import logging
+import json
+from lastfm_api import *
+
+
+class LastFmWrapper:
+    def __init__(self):
+        logging.info("Initializing Last.fm...")
+        self.api = LastfmAPI()
+
+    def get_now_playing(self, user_name):
+        try:
+            recent_tracks = self.api.get_recent_tracks(user_name,limit=1)
+            if len(recent_tracks) > 0:
+                return recent_tracks[0]
+        except:
+            return None
+
+    def get_recent_tracks(self, user_name, limit=1000):
+        try:
+            recent_tracks = self.api.get_recent_tracks(user_name, limit=limit)
+            if len(recent_tracks) > 0:
+                return recent_tracks
+            else:
+                return None
+        except:
+            return None
+
+
+    def get_user(self, user_name):
+        try:
+            return self.api.get_user_info(user_name)
+        except:
+            return None
+
+    def get_user_artistcount(self, user_name):
+        try:
+            return self.api.get_user_artists(user_name, limit=1).total_artists
+        except:
+            return 0
+
+    def get_user_albumcount(self, user_name):
+        try:
+            return self.api.get_user_albums(user_name, limit=1).total_albums
+        except:
+            return 0
+
+
+    def get_user_artists(self, user_name, limit=None):
+        try:
+            return self.api.get_user_artists(user_name,limit)
+        except:
+            return None
+
+    def get_user_albums(self, user_name, limit=None):
+        try:
+            return self.api.get_user_albums(user_name, limit)
+        except:
+            return None
+
+    def get_user_tags(self, user_name):
+        try:
+            return self.api.get_user_tags(user_name)
+        except:
+            return None
