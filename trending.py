4adfd984edc92f0394aa79b3d328e46ef1131945
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-20 06:14:48 -0400

    Fix syntax error

--- trending.py
+++ trending.py
@@ -2,30 +2,28 @@ import mysql.connector
 import time
 from mysql.connector import errorcode
 
-
 def find_trending_artists(num_days):
-DB_NAME = 'scrobbles'
+    DB_NAME = 'scrobbles'
 
-cnx = mysql.connector.connect(user='root', database=DB_NAME, password='Reverie42!')
-cursor = cnx.cursor()
+    cnx = mysql.connector.connect(user='root', database=DB_NAME, password='Reverie42!')
+    cursor = cnx.cursor()
 
-time_cap = str(time.time() - 86400 * num_days)
+    time_cap = str(time.time() - 86400 * num_days)
 
-select_artists = ("SELECT artist FROM scrobbles "
-                  "WHERE timestamp > " + time_cap)
+    select_artists = ("SELECT artist FROM scrobbles "
+                      "WHERE timestamp > " + time_cap)
 
-cursor.execute(select_artists)
+    cursor.execute(select_artists)
 
-trending_artist_dict = {}
-for artist in cursor:
-    if artist[0] in trending_artist_dict:
-        trending_artist_dict[artist[0]] += 1
-    else:
-        trending_artist_dict[artist[0]] = 1
+    trending_artist_dict = {}
+    for artist in cursor:
+        if artist[0] in trending_artist_dict:
+            trending_artist_dict[artist[0]] += 1
+        else:
+            trending_artist_dict[artist[0]] = 1
 
-cnx.commit()
-cursor.close()
-cnx.close()
+    cnx.commit()
+    cursor.close()
+    cnx.close()
 
-
-print(trending_artist_dict)
+    return trending_artist_dict
