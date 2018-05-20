6e953b25a27cba407903590e321fc08de6002393
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-19 21:05:46 -0400

    Troubleshoot

--- trending.py
+++ trending.py
@@ -2,29 +2,29 @@ import mysql.connector
 from mysql.connector import errorcode
 
 DB_NAME = 'scrobbles'
+NUM_DAYS = 1
 
-def trending_artists(num_days):
-    cnx = mysql.connector.connect(user='root', database=DB_NAME, password='Reveri42!')
-    cursor = cnx.cursor()
+cnx = mysql.connector.connect(user='root', database=DB_NAME, password='Reveri42!')
+cursor = cnx.cursor()
 
-    time_cap = str(time.time() - 86400 * num_days)
+time_cap = str(time.time() - 86400 * NUM_DAYS)
 
-    select_artists = ("SELECT artist FROM scrobbles "
-                      "WHERE timestamp > " + time_cap)
+select_artists = ("SELECT artist FROM scrobbles "
+                  "WHERE timestamp > " + time_cap)
 
-    cursor.execute(select_artists)
-    cnx.commit()
-    
-    trending_artist_dict = {}
-    for i in range(cursor.rowcount):
-        row = cursor.fetchone()
-        if row[0] in trending_artist_dict:
-            trending_artist_dict[row[0]] += 1
-        else:
-            row[0] = 1
+cursor.execute(select_artists)
+cnx.commit()
 
-    cursor.close()
-    cnx.close()
+trending_artist_dict = {}
+for i in range(cursor.rowcount):
+    row = cursor.fetchone()
+    if row[0] in trending_artist_dict:
+        trending_artist_dict[row[0]] += 1
+    else:
+        row[0] = 1
+
+cursor.close()
+cnx.close()
 
 
-print(trending_artists(1))
+print(trending_artist_dict)
