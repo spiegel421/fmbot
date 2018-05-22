ae1bc177307186eae669156dd7ae8e67161cebb4
Justin Spiegel <justin@Justins-Air.home>
2018-05-21 23:30:10 -0400

    Add joke in separate file

---
+++ joke.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from random import randrange
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author.id == '206277069678575616':
+        r = randrange(0, 1)
+        if r == 0:
+            await client.send_message(message.channel, 'Shut the fuck up, <@206277069678575616>.')
+
+client.run('NDQ1ODQzODMwODYwOTM5MjY1.DdzE-g.kffUonxFS9M-0OMCUcwnAYErGYQ')
