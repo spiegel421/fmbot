96b153388d511a62088a8fa659f6c2b5a2cae99b
Justin Spiegel <justin@Justins-Air.home>
2018-05-21 23:35:25 -0400

    Finalize joke

--- joke.py
+++ joke.py
@@ -7,7 +7,7 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     if message.author.id == '206277069678575616':
-        r = randrange(0, 1)
+        r = randrange(0, 100)
         if r == 0:
             await client.send_message(message.channel, 'Shut the fuck up, <@206277069678575616>.')
 
