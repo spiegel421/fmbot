010d98a093d33f70748f8ef8601a51d326ec8545
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 15:08:38 -0400

    Troubleshoot

--- webcrawler/webcrawler/retrievers.py
+++ webcrawler/webcrawler/retrievers.py
@@ -2,7 +2,7 @@ import scrapy, sys
 from os.path import dirname
 from scrapy import settings
 from scrapy.crawler import CrawlerProcess
-from spiders import username, top_ratings
+from spiders import username_spider, top_ratings_spider
 from multiprocessing import Process
 
 sys.path.append(dirname("/Users/justin/Documents/lastfm/webcrawler/"))
@@ -11,7 +11,7 @@ s.setmodule('webcrawler.settings')
 
 def check_valid_username(username):
     def f():
-        username_spider = username.UsernameSpider()
+        username_spider = username_spider.UsernameSpider()
         process = CrawlerProcess()
         process.crawl(username_spider, username=username)
         process.start()
@@ -25,7 +25,7 @@ def check_valid_username(username):
 
 def get_top_ratings(username, genre):    
     def f():
-        top_ratings_spider = top_ratings.TopRatingsSpider()
+        top_ratings_spider = top_ratings_spider.TopRatingsSpider()
         process = CrawlerProcess(s)
         process.crawl(top_ratings_spider, username=username)
         process.start()
