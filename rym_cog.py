7126d81a95fcf63520787c6fdaad7c6f2f2dbacf
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 01:38:26 -0400

    Comment unnecessary perms code

--- rym_cog.py
+++ rym_cog.py
@@ -22,8 +22,8 @@ class RYMCog:
 
     @rym.command(pass_context=True)
     async def set(self, ctx, username):
-        if ctx.message.channel != self.bot.get_channel('245685218055290881'):
-            return
+ #       if ctx.message.channel != self.bot.get_channel('245685218055290881'):
+ #           return
 
         rym_data.add_username(ctx.message.author.id, username)
         await self.bot.say("I love you.")
