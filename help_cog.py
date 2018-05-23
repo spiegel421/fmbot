c23b367af84687539667874eb8dbd81e6581fcb5
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 08:46:03 -0400

    Clean

--- help_cog.py
+++ help_cog.py
@@ -13,10 +13,10 @@ class HelpCog:
 
     @help.command()
     async def fm(self):
-        line1 = "`last.fm commands.\n`"
+        line1 = "`last.fm commands.'\n"
         line2 = "`$fm: displays your last played track.`\n"
-        line3 = "`$fm set <username>: sets your last.fm username.\n`"
-        line4 = "`$fm topartists: displays your top artists on last.fm.\n`"
+        line3 = "`$fm set <username>: sets your last.fm username.'\n"
+        line4 = "`$fm topartists: displays your top artists on last.fm.`\n"
         line5 = "`$fm trendingartists <num_days>: displays server's top artists in past num_days.`"
 
         await self.bot.say(line1 + line2 + line3 + line4 + line5)
