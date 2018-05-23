3c6165c7501a1993a5c5289624689efe31d50608
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 08:43:41 -0400

    Add help cog

---
+++ help_cog.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class HelpCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(pass_context=True)
+    async def help(self, ctx):
+        if ctx.invoked_subcommand is None:
+            msg = "`Use $help <cog> for help with a particular cog. Cogs are fm and rym.`"
+            await self.bot.say(msg)
+
+    @help.command()
+    async def fm(self):
+        line1 = "`last.fm commands.\n`"
+        line2 = "`$fm: displays your last played track.`\n"
+        line3 = "`$fm set <username>: sets your last.fm username.\n`"
+        line4 = "`$fm topartists: displays your top artists on last.fm.\n`"
+        line5 = "`$fm trendingartists <num_days>: displays server's top artists in past num_days.`"
+
+        await bot.say(line1 + line2 + line3 + line4 + line5)
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
--- run.py
+++ run.py
@@ -6,31 +6,13 @@ the RateYourMusic scrapy webcrawler.
 """
 import discord
 from discord.ext import commands
-import fm_cog, rym_cog
+import fm_cog, rym_cog, help_cog
 
 bot = commands.Bot(command_prefix='$')
 bot.remove_command('help')
 fm_cog.setup(bot)
 rym_cog.setup(bot)
-
-@bot.event
-async def on_message(message):
-    if message.content.lower().startswith('&help'):
-        commands={}
-        commands['&fm'] = 'Displays your most recently scrobbled track'
-        commands['&fm set <username>'] = 'Sets your lastfm username'
-        commands['&fm topartists'] = 'Displays your top artists on lastfm'
-        commands['&fm trendingartists <num_days>'] = 'Displays the most scrobbled artists on the server within the past num_days'
-        commands['&rym'] = 'Gives the link to your RYM account'
-        commands['&rym set <RYM username>'] = 'Sets the link to your RYM account'
-        commands['&rym get <Discord username>'] = 'Finds the RYM account of another user'
-        
-        embed = discord.Embed(title='Sean', description="Written by Justin S",color=0x0000ff)
-        for command,description in commands.items():
-            embed.add_field(name=command, value=description, inline=False)
-        await bot.say(embed=embed)
-    else:
-        await bot.process_commands(message)
+help_cog.setup(bot)
 
 with open('token.txt', 'r') as reader:
     bot.run(reader.read()[:-1])
