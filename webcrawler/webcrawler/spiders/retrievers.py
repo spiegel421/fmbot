fcb6e9efa7a3f963ac509c9abb2dc7f914d815b3
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 16:05:59 -0400

    Troubleshoot & merge spider files

--- webcrawler/webcrawler/spiders/retrievers.py
+++ webcrawler/webcrawler/spiders/retrievers.py
@@ -3,8 +3,22 @@ from os.path import dirname
 from scrapy import settings
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
-import username_spider, top_ratings_spider
 from multiprocessing import Process
+
+class UsernameSpider(scrapy.Spider):
+    name = 'checkvalidusername'
+    handle_httpstatus_list = [404] 
+    allowed_domains = ['rateyourmusic.com/']
+
+    def __init__(self, username='', **kwargs):
+        self.start_urls = ["https://rateyourmusic.com/~"+username]
+        super().__init__(**kwargs)
+ 
+    def parse(self, response):
+        writer = open("temp.txt", 'w')
+        writer.write(str(response.status))
+        writer.close()
+
 
 def check_valid_username(username):
     def f():
@@ -19,6 +33,23 @@ def check_valid_username(username):
 
     with open("temp.txt", 'r') as reader:
         return reader.read()
+
+class TopRatingsSpider(scrapy.Spider):
+    name = 'gettopratings'
+    allowed_domains = ['rateyourmusic.com/']
+
+    def __init__(self, username='', genre='', **kwargs):
+        self.start_urls = ["https://rateyourmusic.com/collection/"+username+"/strm_h,ss.rd/"+genre]
+        super().__init__(**kwargs)
+ 
+    def parse(self, response):
+        body = response.css("table.mgen tbody")
+        for rating in body.css("tr")[1:]:
+            yield {
+                'artist': rating.css("a.artist::text").extract_first(),
+                'album': rating.css("a.album::text").extract_first(),
+                'rating': rating.css("td.or_q_rating_date_s img::attr(title)").extract_first(),
+                }
 
 def get_top_ratings(username, genre):    
     def f():
