bcabce8eecbee381e60a3a558e915033b873180c
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 22:25:03 -0400

    Troubleshoot

--- webcrawler/webcrawler/pipelines.py
+++ webcrawler/webcrawler/pipelines.py
@@ -7,29 +7,17 @@
 from scrapy.exporters import CsvItemExporter
 
 class TopRatingsPipeline(object):
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self):
+        self.file_name = "webcrawler/webcrawler/temp.csv"
         self.file_handle = None
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        output_file_name = "webcrawler/webcrawler/temp.csv"
-        return cls(output_file_name)
 
     def open_spider(self, spider):
         file = open(self.file_name, 'wb')
         self.file_handle = file
 
-        self.exporter = CsvItemExporter(file)
-        self.exporter.start_exporting()
-
     def close_spider(self, spider):
-        print('Custom Exporter closed')
-
-        self.exporter.finish_exporting()
         self.file_handle.close()
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
         self.file.write(item)
         return item
