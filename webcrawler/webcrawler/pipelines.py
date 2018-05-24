472a62e4e642b538e5f23c167e7b18b04e88c85e
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 21:29:47 -0400

    Fix import error

--- webcrawler/webcrawler/pipelines.py
+++ webcrawler/webcrawler/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy.exporters import CsvItemExporter
 
 class CsvPipeline(object):
     def __init__(self):
