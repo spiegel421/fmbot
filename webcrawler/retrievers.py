3c213ffcbd61bcdffcb3ebb06ead3cae65b630b6
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 21:24:20 -0400

    Troubleshoot

--- webcrawler/retrievers.py
+++ webcrawler/retrievers.py
@@ -1,15 +1,18 @@
 import scrapy, sys, os
 from os.path import dirname
-from scrapy import settings
+from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerProcess
 from webcrawler.webcrawler.spiders import username_spider, top_ratings_spider
 from multiprocessing import Process
 
+s = Settings()
+s.setmodule("webcrawler.webcrawler.settings")
+
 def check_valid_username(username):
     def f():
         u_s = username_spider.UsernameSpider()
-        process = CrawlerProcess(get_project_settings())
+        process = CrawlerProcess(s)
         process.crawl(u_s, username=username)
         process.start()
 
@@ -23,7 +26,7 @@ def check_valid_username(username):
 def get_top_ratings(username, genre):    
     def f():
         t_r_s = top_ratings_spider.TopRatingsSpider()
-        process = CrawlerProcess(get_project_settings())
+        process = CrawlerProcess(s)
         process.crawl(t_r_s, username=username)
         process.start()
 
