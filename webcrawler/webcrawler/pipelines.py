253c460d6b9bc47ea97508e67d8055d8280dea37
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 22:13:41 -0400

    Modify pipeline file

--- webcrawler/webcrawler/pipelines.py
+++ webcrawler/webcrawler/pipelines.py
@@ -6,17 +6,29 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exporters import CsvItemExporter
 
-class CsvPipeline(object):
-    def __init__(self):
-        self.file = open("webcrawler/webcrawler/temp.csv", 'wb')
+class TopRatingsPipeline(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_handle = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        output_file_name = "webcrawler/webcrawler/temp.csv"
+        return cls(output_file_name)
 
     def open_spider(self, spider):
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.start_exporting()
- 
-    def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
+        file = open(self.file_name, 'wb')
+        self.file_handle = file
 
-    def export_item(self, item):
-        self.file.write(item+"\n")
+        self.exporter = CsvItemExporter(file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        print('Custom Exporter closed')
+
+        self.exporter.finish_exporting()
+        self.file_handle.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
