8867833c216717064fc95a97bd6b0aa3adac42b5
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 21:16:50 -0400

    Troubleshoot

--- webcrawler/webcrawler/pipelines.py
+++ webcrawler/webcrawler/pipelines.py
@@ -6,6 +6,16 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class WebcrawlerPipeline(object):
+class CsvPipeline(object):
+    def __init__(self):
+        self.file = open("temp.csv", 'wb')
+        self.exporter = CsvItemExporter(self.file, unicode)
+        self.exporter.start_exporting()
+ 
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+ 
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
