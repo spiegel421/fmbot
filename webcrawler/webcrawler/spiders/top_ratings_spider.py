ef5fafc20a772e62d87810aad3b36cc333a9951c
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-23 23:00:10 -0400

    Troubleshoot

--- webcrawler/webcrawler/spiders/top_ratings_spider.py
+++ webcrawler/webcrawler/spiders/top_ratings_spider.py
@@ -12,9 +12,9 @@ class TopRatingsSpider(scrapy.Spider):
  
     def parse(self, response):
         writer = open("webcrawler/webcrawler/temp.csv", 'w')
-        body = response.css("table.mgen").extract()
+        body = response.css("table.mgen tbody")
         print(len(body))
-        for rating in body.css("tr")[1:]:
+        for rating in body.css("tr").extract()[1:]:
             dict_obj = {
                 'artist': rating.css("a.artist::text").extract_first(),
                 'album': rating.css("a.album::text").extract_first(),
