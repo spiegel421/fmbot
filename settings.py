5de65b1b863829050f39a0d384ff5c7283e854f5
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-24 02:56:57 -0400

    Add retrievers

--- settings.py
+++
@@ -1,8 +0,0 @@
-DOWNLOAD_DELAY = 10
-CONCURRENT_REQUESTS = 1
-ROBOTSTXT_OBEY = False
-REDIRECT_ENABLED = True
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-ITEM_PIPELINES = {
-    'lastfm_test.pipelines.CsvPipeline': 500,
-    }
