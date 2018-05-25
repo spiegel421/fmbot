0a0596ab1d752880983b236799dcbd19d453025a
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-25 08:37:30 -0400

    Start on AWOL cog

---
+++ awol_cog.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from datetime import datetime, timedelta
+
+class AWOLCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    @commands.cooldown(1, 86400, commands.BucketType.server)
+    async def awol(self):
+        for member in self.bot.get_all_members():
+            for channel in self.bot.get_all_channels():
+                has_sent_message = False
+                time = datetime.now() - timedelta(days=14)
+                for message in channel.history(after=time):
+                    if member == message.author:
+                        has_sent_message = True
+                        break
+                if has_sent_message:
+                    break
+            if not has_sent_message:
+                awol_role = discord.utils.get(self.bot.get_server('243129311421399050').role_hierarchy, id='449549299681067010')
+                self.bot.add_roles(member, awol_role)
+
+def setup(bot):
+    bot.add_cog(AWOLCog(bot))
--- run.py
+++ run.py
@@ -6,13 +6,14 @@ the RateYourMusic scrapy webcrawler.
 """
 import discord
 from discord.ext import commands
-import fm_cog, rym_cog, help_cog
+import fm_cog, rym_cog, help_cog, awol_cog
 
 bot = commands.Bot(command_prefix='$')
 bot.remove_command('help')
 fm_cog.setup(bot)
 rym_cog.setup(bot)
 help_cog.setup(bot)
+awol_cog.setup(bot)
 
 with open('token.txt', 'r') as reader:
     bot.run(reader.read()[:-1])
