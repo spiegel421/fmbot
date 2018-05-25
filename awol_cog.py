525f61243f14e158cf17a1b37649b87a0eae3f23
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-25 08:53:11 -0400

    Fix errors

--- awol_cog.py
+++ awol_cog.py
@@ -9,15 +9,15 @@ class AWOLCog:
     @commands.command(pass_context=True)
     @commands.cooldown(1, 86400, commands.BucketType.server)
     async def awol(self, ctx):
+        time = datetime.now() - timedelta(days=14)
         for member in self.bot.get_all_members():
-            for channel in self.bot.get_all_channels():
-                has_sent_message = False
-                time = datetime.now() - timedelta(days=14)
-                for message in commands.Context.history(after=time):
-                    if member == message.author:
-                        has_sent_message = True
-                        break
-                if has_sent_message:
+            has_sent_message = False
+            messages = discord.utils.get(self.bot.messages, author=member)
+            for message in messages:
+                if message.timestamp < time:
+                    break
+                elif member == message.author:
+                    has_sent_message = True
                     break
             if not has_sent_message:
                 awol_role = discord.utils.get(self.bot.get_server('243129311421399050').role_hierarchy, id='449549299681067010')
