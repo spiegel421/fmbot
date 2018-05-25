043a28ec19ec7d583648587a87d3f6b5ad3b56fe
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-25 18:37:28 -0400

    Troubleshoot

--- awol_cog.py
+++ awol_cog.py
@@ -22,6 +22,7 @@ class AWOLCog:
                 async for message in self.bot.logs_from(channel, after=time):
                     if message.author in members:
                         members.remove(message.author)
+                print('good')
             except:
                 print('sorry')
 
