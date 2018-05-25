7cdcdee166df9e8ca8b968cb36e9b88603a89bae
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-25 17:57:30 -0400

    Troubleshoot

--- awol_cog.py
+++ awol_cog.py
@@ -12,19 +12,16 @@ class AWOLCog:
         time = datetime.now() - timedelta(days=14)
         members = []
         for member in self.bot.get_all_members():
-            regular = discord.utils.get(member.server.roles, name="Regular")
-            if regular in member.roles:
-                members.append(member)
+ #           regular = discord.utils.get(member.server.roles, name="Regular")
+#            if regular in member.roles:
+            members.append(member)
         for channel in self.bot.get_all_channels():
-            try:
-                async for message in self.bot.logs_from(channel, after=time):
-                    if message.author in members:
-                        members.remove(message.author)
-            except:
-                continue
+            async for message in self.bot.logs_from(channel, after=time):
+                if message.author in members:
+                    members.remove(message.author)
         print(len(members))
         for member in members:
-            awol = discord.utils.get(member.server.roles, name="AWOL")
+            awol = discord.utils.get(member.server.roles, name="awol")
             await self.bot.add_roles(member, awol)
 
 
