36e1ad395e712e50489ceca4650e5d818218f936
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-25 08:54:24 -0400

    Fix errors

--- awol_cog.py
+++ awol_cog.py
@@ -13,15 +13,18 @@ class AWOLCog:
         for member in self.bot.get_all_members():
             has_sent_message = False
             messages = discord.utils.get(self.bot.messages, author=member)
-            for message in messages:
-                if message.timestamp < time:
-                    break
-                elif member == message.author:
-                    has_sent_message = True
-                    break
-            if not has_sent_message:
-                awol_role = discord.utils.get(self.bot.get_server('243129311421399050').role_hierarchy, id='449549299681067010')
-                self.bot.add_roles(member, awol_role)
+            try:
+                for message in messages:
+                    if message.timestamp < time:
+                        break
+                    elif member == message.author:
+                        has_sent_message = True
+                        break
+                if not has_sent_message:
+                    awol_role = discord.utils.get(self.bot.get_server('243129311421399050').role_hierarchy, id='449549299681067010')
+                    self.bot.add_roles(member, awol_role)
+            except:
+                continue
 
 def setup(bot):
     bot.add_cog(AWOLCog(bot))
