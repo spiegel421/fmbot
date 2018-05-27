6b90e319ca87355d7c702920443873fc2407c169
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-27 08:06:55 -0400

    Troubleshoot

--- lastfm_api.py
+++ lastfm_api.py
@@ -96,7 +96,7 @@ class LastfmAPI:
                 logging.error(parsed_json)
                 return None
             else:
-                artist_tracks= parsed_json['artisttracks']['track']['artist']
+                artist_tracks= parsed_json['artisttracks']['track']
                 num_scrobbles = len(artist_tracks)
 
             return num_scrobbles
