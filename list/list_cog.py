e3d27a273c91fd0519ab1fa9353b7c4c911de8e6
Justin Spiegel <justin@Justins-Air.home>
2018-05-27 16:25:44 -0400

    Troubleshoot

--- list/list_cog.py
+++ list/list_cog.py
@@ -9,20 +9,28 @@ class ListCog:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def list(self, ctx, *, args):
+    async def list(self, ctx, *args):
         user_lists = list_dict = None
         if len(ctx.message.mentions) == 1:
             discord_id = ctx.message.mentions[0].id
             if len(args) == 1:
                 user_lists = list_data.get_user_lists(discord_id)
             else:
-                list_dict = list_data.get_list(discord_id, args)
+                list_name = ""
+                for arg in args[1:]:
+                    list_name += arg + " "
+                list_name = list_name[:-1]
+                list_dict = list_data.get_list(discord_id, list_name)
         elif len(ctx.message.mentions) == 0:
             discord_id = ctx.message.author.id
             if len(args) == 0:
                 user_lists = list_data.get_user_lists(discord_id)
             else:
-                list_dict = list_data.get_list(discord_id, args)
+                list_name = ""
+                for arg in args[1:]:
+                    list_name += arg + " "
+                list_name = list_name[:-1]
+                list_dict = list_data.get_list(discord_id, list_name)
 
         description = ""
         if user_lists is not None:
