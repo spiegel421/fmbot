c96a1042e6bcc58448c9985bfef6c30e5e93f7cc
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-27 08:23:16 -0400

    Add name in scrobbles command

--- fm_cog.py
+++ fm_cog.py
@@ -66,7 +66,7 @@ class FmCog:
         artist_name = artist_name[:-1]
 
         play_count = str(self.lastfm.get_user_numscrobbles(username, artist_name))
-        await self.bot.say("User has scrobbled "+artist_name+" "+play_count+" times.")
+        await self.bot.say(ctx.message.author.name+" has scrobbled "+artist_name+" "+play_count+" times.")
 
     @fm.command(pass_context=True)
     async def trendingartists(self, ctx, num_days):
