3bdee76570a8bd4a4dc37827440f5188789a8bc1
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-05-29 18:20:40 -0400

    Fix error

--- list/list_data.py
+++ list/list_data.py
@@ -37,7 +37,7 @@ cnx.close()
 
 def create_list(discord_id, list_name):
     cnx = mysql.connector.connect(user='root', database=DB_NAME, password='Reverie42!')
-    cursor = cnx.cursor()
+    cursor = cnx.cursor(buffered=True)
     
     name = discord_id + "_" + list_name
     create = (
