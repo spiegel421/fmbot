fdce16fa9a4b299e2408c91f2b54d142d8567066
Justin Spiegel <justin@Justins-MacBook-Air.local>
2018-06-07 17:08:09 -0400

    Clean

--- awol/awol_data.py
+++ awol/awol_data.py
@@ -52,7 +52,7 @@ def get_awol_users():
     cnx = mysql.connector.connect(user='root', database=DB_NAME, password='Reverie42!')
     cursor = cnx.cursor(buffered=True)
 
-    time = datetime.now() - timedelta(weeks=2)
+    time = datetime.now() - timedelta(weeks=1)
     get_awol_users = ("SELECT discord_id FROM timestamps "
                     "WHERE timestamp < '" + str(time) + "'")
     
